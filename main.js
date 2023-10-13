83b12cd09995d0b60194391a711f76cb358e8a3c
Ignaciocingolani <ignaciocingolani98@gmail.com>
2023-10-13 10:51:11 -0300

    correccion objetos y arrays

--- main.js
+++ main.js
@@ -1,62 +1,53 @@
-const bienvenida = (nombre) =>
-alert ("Bienvenido "+ nombre + " al simulador de plazos fijos");
-
-bienvenida( prompt("Ingresa tu nombre."));
-
-let valor = 0;
-let meses = 0;
-let otraOperacion = false;
 
 
+const saludar = (nombre) => {
+    alert("¡Hola, " + nombre + "! Bienvenido al simulador de plazos fijos");
+};
 
-function calculadorInteres () {
-    do{
-        valor= parseInt(prompt("Ingresa monto a invertir"));
-        meses= parseInt(prompt("Ingresa cantidad de meses a invertir; 6, 12, 18, 24"));
-        intereses (0.1, 0.2, 0.3, 0.5);
-        otraOperacion= confirm( "Deseas realizar otra operacion?");
-        
-    } while (otraOperacion);
+
+saludar(prompt("Ingresa tu nombre."));
+
+
+const inversiones = []; 
+function calculadorInteres() {
+    do {
+        const inversion = {}; 
+        inversion.monto = parseFloat(prompt("Ingresa monto a invertir"));
+        inversion.meses = parseInt(prompt("Ingresa cantidad de meses a invertir; 6, 12, 18, 24"));
+        inversion.resultado = intereses(inversion.monto, inversion.meses);
+        inversiones.push(inversion); 
+        alert(`Quieres invertir ${inversion.monto} pesos con una ganancia total de ${inversion.resultado}`);
+    } while (confirm("¿Deseas realizar otra operación?"));
+
     
-
+    document.write("<h2>Resumen de inversiones:</h2>");
+    for (const inversion of inversiones) {
+        document.write(`Inversión de $${inversion.monto} a ${inversion.meses} meses: Resultado = $${inversion.resultado}<br>`);
+    }
 }
-const calculador = [ true, false, true, false]
 
-function intereses (seis, doce, dieciocho, veinticuatro) {
+function intereses(monto, meses) {
     let resultado = 0;
-    let mensaje = " ";
-   
    
     switch (meses) {
         case 6:
-            resultado = ((valor * seis) *6).toFixed(2);
-            mensaje = `Quieres invertir ${valor} pesos con una ganancia total de ${resultado}`;
+            resultado = ((monto * 0.1) * 6).toFixed(2);
             break;
-
         case 12:
-            resultado = ((valor * doce) *12).toFixed(2);
-            mensaje = `Quieres invertir ${valor} pesos con una ganancia total de ${resultado}`;
+            resultado = ((monto * 0.2) * 12).toFixed(2);
             break;
-
         case 18:
-            resultado = ((valor * dieciocho) *18).toFixed(2);
-            mensaje = `Quieres invertir ${valor} pesos con una ganancia total de ${resultado}`;
+            resultado = ((monto * 0.3) * 18).toFixed(2);
             break;
-
         case 24:
-            resultado = ((valor * veinticuatro) *24).toFixed(2);
-            mensaje = `Quieres invertir ${valor} pesos con una ganancia total de ${resultado}`;
+            resultado = ((monto * 0.5) * 24).toFixed(2);
             break;
-
-
         default:
-            mensaje = "Por favor ingrese 6, 12, 18 o 24 meses.";
+            alert("Por favor, ingrese 6, 12, 18 o 24 meses.");
             break;
-
     }
-    
-document.write(`${mensaje}<br>`);
 
+    return resultado;
 }
 
-calculadorInteres ();
+calculadorInteres();
